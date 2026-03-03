f092764cc9216feac850bfd1915ec74d14830792
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-03-03 12:01:52 +0100

    feat(System): add AnimationSystem

---
+++ src/systems/AnimationSystem.cpp
@@ -0,0 +1,23 @@
+/*
+** Prototype-Raylib-3D
+** File description:
+** AnimationSystem
+*/
+
+#include <entt/entt.hpp>
+#include "components/Components.hpp"
+
+void AnimationSystem(entt::registry& registry)
+{
+    auto view = registry.view<ModelRenderer>();
+
+    for (auto entity : view) {
+        auto& renderer = view.get<ModelRenderer>(entity);
+
+        if (renderer.animsCount > 0) {
+            ModelAnimation anim = renderer.modelAnim[renderer.currentAnimIndex];
+            renderer.currentAnimFrame = (renderer.currentAnimFrame + 1) % anim.frameCount;
+            UpdateModelAnimation(renderer.model, anim, renderer.currentAnimFrame);
+        }
+    }
+}
