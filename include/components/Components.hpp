7820aec4f379c5b8eb97a63381e2c5d95a74ff8c
Aymeric <aymeric.jouannet-mimy@epitech.eu>
2026-03-03 11:57:56 +0100

    feat(Components.hpp): add basics Components for the ECS TransformComponent Physics ModelRenderer PlayerTag ObstacleTag ...

---
+++ include/components/Components.hpp
@@ -0,0 +1,75 @@
+/*
+** Prototype-Raylib-3D
+** File description:
+** Components
+*/
+
+#ifndef COMPONENTS_HPP_
+#define COMPONENTS_HPP_
+
+#include <raylib.h>
+
+// ############################################################################
+// ################################## CORE ####################################
+// ############################################################################
+
+/**
+ * @brief Represents the spatial state of an entity in the world.
+ *
+ * @param pos Vector3
+ * @param rotation float
+ * @param scale float
+ */
+struct TransformComponent
+{
+    Vector3 pos;
+    float rotation;
+    float scale;
+};
+
+
+// ############################################################################
+// ################################ PHYSIC ####################################
+// ############################################################################
+
+struct Physics
+{
+    Vector3 velocity;
+    bool isGrounded;
+};
+
+
+// ############################################################################
+// ################################ RENDER  ###################################
+// ############################################################################
+
+/**
+ * @brief Represents the 3d model of an entity in the world.
+ *
+ * @param model Model
+ * @param modelAnim ModelAnimation*
+ * @param animsCount int
+ * @param currentAnimIndex int
+ * @param currentAnimFrame int
+ */
+struct ModelRenderer
+{
+    Model model;
+    ModelAnimation* modelAnim;
+    int animsCount;
+    int currentAnimIndex;
+    int currentAnimFrame;
+};
+
+
+// ############################################################################
+// ################################# TAGS #####################################
+// ############################################################################
+
+
+struct PlayerTag {};
+
+struct ObstacleTag { BoundingBox box; };
+
+
+#endif /* !COMPONENTS_HPP_ */
